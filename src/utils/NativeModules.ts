ba675e834b1c3a1a417cbfe1f9c6cfc460b0691b
Awais Ibrar <awais.ibrar@myalfred.com>
2026-03-12 00:04:48 +0500

    fix: native module name

--- src/utils/NativeModules.ts
+++ src/utils/NativeModules.ts
@@ -1,18 +1,18 @@
-import { NativeModules, Platform } from "react-native";
+import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-    `The package 'react-native-afspay-sdk' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo managed workflow\n';
+  `The package 'react-native-afspay-sdk' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
 
-export const HyperPaySDK = NativeModules.HyperPay
-    ? NativeModules.HyperPay
-    : new Proxy(
-        {},
-        {
-            get() {
-                throw new Error(LINKING_ERROR);
-            },
-        }
-    );
+// Native module is registered as "HyperPay" on both iOS and Android
+const getNativeModule = () => NativeModules?.HyperPay;
+
+export const HyperPaySDK = getNativeModule()
+  ? getNativeModule()
+  : new Proxy({} as any, {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    });
